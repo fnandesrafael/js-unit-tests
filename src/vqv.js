31591731f3d914393f7e82533663b9590e85034e
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-09-07 23:17:32 -0300

    corrige problema de sintaxe nos requisitos 2 e 3

--- src/vqv.js
+++ src/vqv.js
@@ -20,11 +20,11 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  
+
   let hello = `Oi, meu nome é ${nome}
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 
   return hello;
 };
