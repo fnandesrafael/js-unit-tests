9f0d00205b425b9a00d0590a69f8425f411518a4
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-09-08 08:54:47 -0300

    altera valor esperado pelo circle.spec.js

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -41,7 +41,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     /**
      * Os valores tirados para a área e a circunferência vieram do próprio circle.js
      */
-    assert.strictEqual(circle(2).circumference, 12.57);
+    assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     assert.strictEqual(circle(3), 28.259999999999998); 
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
