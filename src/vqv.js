9a5c308b929fe27f117e76ee33bf60dd30cdc684
Rafael de Lima <rafaelima.fernandez@gmail.com>
2021-09-07 23:20:44 -0300

    adiciona comentarios ao arquivo vqv.js

--- src/vqv.js
+++ src/vqv.js
@@ -17,15 +17,17 @@
 */
 
 const vqv = (nome, idade) => {
+// Verifica se os parametros foram definidos na função, caso não ela permanece indefinida
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-
+// Uso de Template Literal para a criação do parágrafo do requisito 3
   let hello = `Oi, meu nome é ${nome}
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
 
+// Retorna o parágrafo
   return hello;
 };
 
